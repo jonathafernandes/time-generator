c29eaa247ecdec2b30ebc327eab1794a5fd00d8b
jonathafernandes <jonathafernandes@live.com>
2024-02-14 22:42:37 -0300

    style: change descripition

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ import createTimes from './src/app.js'
 
 document.querySelector('.container').innerHTML = `
   <div class="form">
+    <p class="title">Crie seus times de futsal!</p>
     <label for="quantity">Digite a quantidade de jogadores em cada time (entre 4 e 5):</label>
     <input type="number" id="quantity" min="4" max="5" value="5">
     <br>    
     <label for="maxValue">Digite o total de jogadores (entre 10 e 25):</label>
     <input type="number" id="maxValue" min="10" max="25" value="25">
 
-    <br>
     <br>
     <br>
     
