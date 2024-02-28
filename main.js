c226bc3503e1843d2670dccdb09ff42b3ca78cba
jonathafernandes <jonathafernandes@live.com>
2024-02-27 22:20:13 -0300

    feat: form custom creation

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import './public/style.css'
 import createTimes from './src/app.js'
 
 const title = document.querySelector('.title');
+const content = document.querySelector('.content');
 const defaultCreation = document.getElementById('defaultCreation');
 const customCreation = document.getElementById('customCreation');
-const content = document.querySelector('.content');
 const backButton = document.getElementById('backButton');
 
 defaultCreation.addEventListener('click', () => {
@@ -38,7 +38,43 @@ customCreation.addEventListener('click', () => {
   customCreation.style.display = 'none';
   backButton.style.display = 'block';
 
-  content.innerHTML = `<h2 data-aos="zoom-out">Em breve...</h2>`;
+  content.innerHTML = `
+    <div data-aos="zoom-out" class="form customCreation">
+      <label for="quantity">Digite a quantidade de jogadores em cada time (entre 4 e 5):</label>
+      <input type="number" id="quantity" min="4" max="5" value="5">
+      <br>    
+      <label for="maxValue">Digite o total de jogadores (entre 10 e 25):</label>
+      <input type="number" id="maxValue" min="10" max="25" value="25">
+
+      <br>
+      <br>
+      
+      <button id="nextButton">Próximo</button>
+    </div>
+
+    <ul id="results"></ul>
+  `;
+
+  content.addEventListener('click', event => {
+    const target = event.target;
+  
+    if (target.id === 'nextButton') {
+      content.innerHTML = `
+        <div data-aos="zoom-out" class="form">
+          <label for="quantity">Digite os nomes dos jogadores e defina um nível técnico entre 1 e 5:</label>
+          <br>
+          <input type="text" id="players" placeholder="Digite o nome">
+          <input type="number" id="level" min="1" max="5" value="5">
+          <br>
+          <br>
+          <button id="createTimesButton">Definir times</button>
+        </div>
+  
+        <ul id="results"></ul>
+      `;
+    }
+  });
+
   createTimes();
 });
 
@@ -50,4 +86,3 @@ backButton.addEventListener('click', () => {
 
   content.innerHTML = '';
 });
-
